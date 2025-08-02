093880306cd8f65f5ad8f295d82d6161fd56f1df
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-02 17:46:30 +0530

    User Details

---
+++ Server/controllers/userDetails.js
@@ -0,0 +1,31 @@
+import userModel from "../models/UserModel.js";
+
+export const getUserData = async (req, res) => {
+    try {
+        
+        const userId = req.userId;
+        const user = await userModel.findById(userId);
+
+        if(!user) {
+            return res.json({
+                message: "User not found", 
+                success: false
+            })
+        }
+
+        res.json({
+            message: "User data retrived successfully",
+            userData: {
+                name: user.name,
+                isAccountVerified: user.isAccountVerified
+            },
+            success: true
+        })
+
+    } catch (error) {
+        return res.json({
+                message: error.message,
+                success: false
+            })
+    }
+}
