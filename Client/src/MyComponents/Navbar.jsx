402fa4f1e682366962ebb6e24fff7aa347622b2e
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-05 13:15:39 +0530

    Navbar.jsx

---
+++ Client/src/MyComponents/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useContext } from "react";
+import { assets } from "../assets/assets.js";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../Context/AppContext.jsx";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const { userData, backendURL, setUserData, setIsLoggedIn } = useContext(AppContext);
+
+  const sendVerificationOtp = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const { data } = await axios.post(
+        backendURL + "/api/user/send-verify-otp"
+      );
+
+      if (data.success) {
+        navigate("/verify-email");
+        toast.success(data.success);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      axios.defaults.withCredentials = true;
+
+      const { data } = await axios.post(backendURL + "/api/user/logout");
+      data.success && setIsLoggedIn(false);
+      data.success && setUserData(false);
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
+      <img src={assets.logo} alt="" className="w-28 sm:w-32 cursor-pointer" />
+      {userData ? (
+        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black cursor-pointer text-white relative group">
+          {userData.name[0].toUpperCase()}
+          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
+            <ul className="list-none m-0 p-2 bg-gray-100 text-sm px-5">
+              {!userData.isAccountVerified && (
+                <li
+                  onClick={sendVerificationOtp}
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                >
+                  Verify Email
+                </li>
+              )}
+              <li
+                onClick={logout}
+                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <button
+          onClick={() => navigate("/login")}
+          className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 cursor-pointer text-gray-800 hover:bg-gray-100 transition-all"
+        >
+          Login <img src={assets.arrow_icon} alt="" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
