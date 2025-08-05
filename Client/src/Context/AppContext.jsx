fb13640e0328822858383504cdd701e05151b5ca
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-05 13:14:49 +0530

    AppContext

---
+++ Client/src/Context/AppContext.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { createContext } from "react";
+import { toast } from "react-toastify";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+
+    axios.defaults.withCredentials = true;
+
+    const backendURL = import.meta.env.VITE_BACKEND_URL
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+    const [userData, setUserData] = React.useState(null);
+
+    const getAuthStatus = async () => {
+        try {
+            const {data} = await axios.get(backendURL + '/api/user/is-auth')
+
+            if(data.success) {
+                setIsLoggedIn(true)
+                getUserData()
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const getUserData = async () => {
+        try {
+            const res = await axios.get(backendURL + '/api/user-details/data', { withCredentials: true });
+            const data = res.data;
+            data.success ? setUserData(data.userData) : toast.error(data.message)
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getAuthStatus();
+    }, [])
+
+    const value = {
+        backendURL,
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData,
+        getUserData
+    }
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
